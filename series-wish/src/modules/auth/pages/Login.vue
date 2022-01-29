64b7d08bb75bd6f16e214fa0e9ce531d8d951296
Diegoarend <diegoaw@gmail.com>
2022-01-29 19:02:44 -0300

    Update Login.vue
    create login form

--- series-wish/src/modules/auth/pages/Login.vue
+++ series-wish/src/modules/auth/pages/Login.vue
@@ -1,15 +1,62 @@
 <template>
-  <div class="login">
-    <h1>login</h1>
-  </div>
+  <form @submit.prevent="submit()">
+    <div class="login-page">
+      <div class="card">
+        <div class="card-header"> 
+          Login 
+        </div>
+        <div class="card-body">
+          <div class="form-group">
+            <input 
+              v-model="form.email"
+              required 
+              type="email"
+              class="form-control" 
+              placeholder="E-mail"
+            >
+          </div>
+          <div class="form-group">
+            <input
+              v-model="form.password"
+              required
+              type="password"
+              class="form-control"
+              placeholder="Senha"
+            >
+          </div>
+          <button class="btn btn-primary w-100">
+            Entrar
+          </button>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
-  
+  data:() => ({
+    form: {
+      email:"",
+      password:""
+    }
+  }),
+  methods: {
+    submit () {
+      console.log(this.form)
+    }
+  }
 }
 </script>
 
-<style lang="scss">
-
+<style scoped lang="scss">
+.login-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .card{
+    width: 30%;
+  }
+}
 </style>
